14c7117566d155bb0b799d0d8f51764ad416daa2
Maia <maia.rt.46@gmail.com>
2017-05-30 15:49:23 -0500

    Subiendo Js de calcular Ruta

--- js/app.js
+++ js/app.js
@@ -1,35 +1,34 @@
 function initMap() {
-  var ubicacion;
-  var coordsPeru = {lat: -9.082632, lng: -84.0431127};
+  var latitud,longitud, marcador;
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
-    center: coordsPeru
+    center: {lat: -9.082632, lng: -84.0431127}//Coordenadas de Peru
   });
 
-  var latitud,longitud;
-
-  var encontrar = function(posicion) {
+  var miUbicacion = function(posicion) {
     latitud = posicion.coords.latitude;
     longitud = posicion.coords.longitude;
 
-    ubicacion = new google.maps.Marker({
+    marcador = new google.maps.Marker({
       position: {lat:latitud, lng:longitud},
       animation: google.maps.Animation.DROP,
       map: map
-    });
+  });
     map.setZoom(17);
     map.setCenter({lat:latitud, lng:longitud});
   }
   var error = function (error) {
     window.alert("Tu navegador no soporta la API de geolocalizacion");
   }
+
   function buscar(e) {
     e.preventDefault();
     if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(encontrar,error);
+      navigator.geolocation.getCurrentPosition(miUbicacion,error);
     }
   }
 
+// Trazando ruta
   var puntoPartida = document.getElementById("puntoPartida");
   var puntoDestino = document.getElementById("puntoDestino");
   new google.maps.places.Autocomplete(puntoPartida);
@@ -56,7 +55,7 @@ function initMap() {
       }
     });
     direccionDisplay.setMap(map);
-    ubicacion.setMap(null);
+    marcador.setMap(null);
   }
 
   window.addEventListener("load",buscar);
@@ -64,5 +63,4 @@ function initMap() {
     e.preventDefault();
     calcularRuta(direccionService, direccionDisplay);
   });
-
 }
