420d9a147d7cb4b03577986fd765be0970fc1c9b
Maia <maia.rt.46@gmail.com>
2017-05-30 14:05:28 -0500

    calculando y trazando ruta con js

--- js/app.js
+++ js/app.js
@@ -1,66 +1,68 @@
 function initMap() {
-    var coordsPeru = {lat: -9.082632, lng: -84.0431127};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: coordsPeru
+  var ubicacion;
+  var coordsPeru = {lat: -9.082632, lng: -84.0431127};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: coordsPeru
+  });
+
+  var latitud,longitud;
+
+  var encontrar = function(posicion) {
+    latitud = posicion.coords.latitude;
+    longitud = posicion.coords.longitude;
+
+    ubicacion = new google.maps.Marker({
+      position: {lat:latitud, lng:longitud},
+      animation: google.maps.Animation.DROP,
+      map: map
     });
-
-  	var latitud,longitud;
-
-    var encontrar = function(posicion) {
-  		latitud = posicion.coords.latitude;
-  		longitud = posicion.coords.longitude;
-
-  		var ubicacion = new google.maps.Marker({
-  			position: {lat:latitud, lng:longitud},
-  			animation: google.maps.Animation.DROP,
-  			map: map
-  		});
-    		map.setZoom(17);
-    		map.setCenter({lat:latitud, lng:longitud});
-    	}
-    	var error = function (error) {
-    		window.alert("Tu navegador no soporta la API de geolocalizacion");
-    	}
-      function buscar(e) {
-        e.preventDefault();
-        if(navigator.geolocation){
-          navigator.geolocation.getCurrentPosition(encontrar,error);
-        }
-      }
-
-    var puntoPartida = document.getElementById("puntoPartida");
-    var puntoDestino = document.getElementById("puntoDestino");
-    new google.maps.places.Autocomplete(puntoPartida);
-    new google.maps.places.Autocomplete(puntoDestino);
-
-    var direccionService = new google.maps.DirectionsService;
-    var direccionDisplay =new google.maps.DirectionsRenderer;
-
-    var calcularRuta = function (direccionService, direccionDisplay) {
-      var request = {
-        origin: puntoPartida.value,
-        destination: puntoDestino.value,
-        travelMode: 'DRIVING'
-        };
-        direccionService.route(request, function(result, status) {
-        if (status == 'OK') {
-          var distancia = result.routes[0].legs[0].distance.value/1000;
-          var duracion = result.routes[0].legs[0].duration.text;
-          var costo = (distancia*1.75).toFixed(2);
-
-          document.getElementById('calcTarifa').innerHTML="";
-          document.getElementById('calcTarifa').innerHTML=`Costo: S/. ${costo} <br> Duración: ${duracion}`;
-          direccionDisplay.setDirections(result);
-        }
-      });
-      direccionDisplay.setMap(map);
+    map.setZoom(17);
+    map.setCenter({lat:latitud, lng:longitud});
+  }
+  var error = function (error) {
+    window.alert("Tu navegador no soporta la API de geolocalizacion");
+  }
+  function buscar(e) {
+    e.preventDefault();
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(encontrar,error);
     }
+  }
 
-    window.addEventListener("load",buscar);
-    document.getElementById('trazarRuta').addEventListener("click",function(e){
-      e.preventDefault();
-      calcularRuta(direccionService, direccionDisplay);
+  var puntoPartida = document.getElementById("puntoPartida");
+  var puntoDestino = document.getElementById("puntoDestino");
+  new google.maps.places.Autocomplete(puntoPartida);
+  new google.maps.places.Autocomplete(puntoDestino);
+
+  var direccionService = new google.maps.DirectionsService;
+  var direccionDisplay = new google.maps.DirectionsRenderer;
+
+  var calcularRuta = function (direccionService, direccionDisplay) {
+    var request = {
+      origin: puntoPartida.value,
+      destination: puntoDestino.value,
+      travelMode: 'DRIVING'
+    };
+    direccionService.route(request, function(result, status) {
+      if (status == 'OK') {
+        var distancia = result.routes[0].legs[0].distance.value/1000;
+        var duracion = result.routes[0].legs[0].duration.text;
+        var costo = (distancia*1.75).toFixed(2);
+
+        document.getElementById('calcTarifa').innerHTML="";
+        document.getElementById('calcTarifa').innerHTML=`Costo: S/. ${costo} <br> Duración: ${duracion}`;
+        direccionDisplay.setDirections(result);
+      }
     });
+    direccionDisplay.setMap(map);
+    ubicacion.setMap(null);
+  }
+
+  window.addEventListener("load",buscar);
+  document.getElementById('trazarRuta').addEventListener("click",function(e){
+    e.preventDefault();
+    calcularRuta(direccionService, direccionDisplay);
+  });
 
 }
